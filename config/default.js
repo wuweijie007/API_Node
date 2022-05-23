3ca041be13b440a1e24cc76b4b4add3a8a102e7d
wuweijie007 <18312958085@163.com>
2022-05-23 19:20:27 +0800

    init

--- config/default.js
+++ config/default.js
@@ -1,7 +1,7 @@
 const db_config = {
     host: 'localhost', // 主机ip地址
     user: 'root',  // MySQL用户名
-    password: '123456', // MySQL密码
+    password: '123456wu', // MySQL密码
     database: 'cargo_system',  // 要连接的数据库名
 }
 
