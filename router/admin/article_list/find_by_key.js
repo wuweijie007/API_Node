c118dcffda1c61f0bb767ff04bf8796fae6b6e12
wuweijie007 <18312958085@163.com>
2022-06-06 15:29:35 +0800

    hot list 增加limit限制

--- router/admin/article_list/find_by_key.js
+++ router/admin/article_list/find_by_key.js
@@ -7,7 +7,8 @@ module.exports = (req, res, next) => {
     sqlQuery(`SELECT art.id,art.title,art.url ,date_format(art.publish_time, '%Y-%m-%d %H:%i:%s') as publish_time,account.type ,art.push_times ,account.account,art.sn FROM wechat_article_list art join wechat_account account 
     on art.__biz = account.__biz
     where account.type= "${type}"
-    order by art.publish_time desc;`)
+    order by art.publish_time desc
+    limit 100;`)
         .then(data => {
             res.send(resultData('查询成功', 200, data))
         })
